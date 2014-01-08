039362ce98e7409f59355b13c924c6816df222a4
Hack Reactor Students <willgester@gmail.com>
2014-01-08 15:44:47 -0800

    Passed initial specs for LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,30 @@ var makeLinkedList = function(){
   list.head = null;
   list.tail = null;
 
+
   list.addToTail = function(value){
+      if (list.head === null){
+        list.tail = makeNode(value);
+        list.head = list.tail;
+      } else {
+        list.tail.next = makeNode(value);
+        list.tail = list.tail.next;
+      }
   };
 
   list.removeHead = function(){
+    var result = list.head;
+    list.head = list.head.next;
+    return result;
   };
 
   list.contains = function(target, node){
+    for( var node in list ){
+      if( list[node].value === target ){
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
