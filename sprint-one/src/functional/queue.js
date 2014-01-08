39d7eeeb23e998866185eeec7cb0e0dcab43cf06
Will Gester <wiilgester@gmail.com>
2014-01-08 11:59:08 -0800

    Finished Queue and deleted memory leaking stored values

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -8,9 +8,21 @@ var makeQueue = function(){
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[size] = value;
+    size ++;
   };
 
   instance.dequeue = function(){
+    var result;
+    if (size > 0){
+      result = storage[0];
+      for (var i = 1; i < size + 1; i ++){
+        storage[i - 1] = storage[i];
+      }
+      delete storage[size - 1];
+      size --;
+    }
+    return result;
   };
 
   instance.size = function(){
