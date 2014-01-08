8341de9b2260be3f703bf3a2ea65ef7e5afcab0a
Will Gester <wiilgester@gmail.com>
2014-01-08 14:16:57 -0800

    Finished extending methods

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,38 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+var makeStack = function(){
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.sizeOf = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+  var extend = function(copyTo, copyFrom) {
+    for (var property in copyFrom) {
+        copyTo[property] = copyFrom[property];
+    }
+};
+extend(instance, stackMethods);
+  
+  return instance;
 };
   
 var stackMethods = {};
+
+ stackMethods.push = function(value){
+    this.storage[this.sizeOf] = value;
+    this.sizeOf += 1;
+  };
+
+  stackMethods.pop = function(){
+    var result;
+    if (this.sizeOf > 0){
+      result = this.storage[this.sizeOf - 1];
+      delete this.storage[this.sizeOf - 1];
+      this.sizeOf --;
+    }
+    return result;
+  };
+
+  stackMethods.size = function(){
+    return this.sizeOf;
+  };
