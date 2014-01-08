03dcf142d8ad58998bfb8a2f5dfe0568bac7974d
Will Gester <wiilgester@gmail.com>
2014-01-08 14:34:02 -0800

    started prototype

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,5 +1,38 @@
-var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var makeQueue = function(){
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.sizeOf = 0;
+
+  // Implement the methods below
+  _.extend(instance, queueMethods);
+
+  
+
+  return instance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+    this.storage[this.sizeOf] = value;
+    this.sizeOf ++;
+  };
+
+  queueMethods.dequeue = function(){
+    var result;
+    if (this.sizeOf > 0){
+      result = this.storage[0];
+      for (var i = 1; i < this.sizeOf + 1; i ++){
+        this.storage[i - 1] = this.storage[i];
+      }
+      delete this.storage[this.sizeOf - 1];
+      this.sizeOf --;
+    }
+    return result;
+  };
+
+  queueMethods.size = function(){
+    return this.sizeOf;
+  };
