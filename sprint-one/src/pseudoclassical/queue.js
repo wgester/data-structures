286d02b3b9960b63e55d38030cd564ceed83e60a
Hack Reactor Students <willgester@gmail.com>
2014-01-08 15:02:13 -0800

    Finished pseudo

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,28 @@
-var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+var Queue = function(){
+  this.storage = {};
+  this.sizeOf = 0;
+
+};
+
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.sizeOf] = value;
+  this.sizeOf ++;
+};
+
+Queue.prototype.dequeue = function(){
+  var result;
+  if (this.sizeOf > 0){
+    result = this.storage[0];
+    for (var i = 1; i < this.sizeOf + 1; i ++){
+      this.storage[i - 1] = this.storage[i];
+    }
+    delete this.storage[this.sizeOf - 1];
+    this.sizeOf --;
+  }
+  return result;
+};
+
+Queue.prototype.size = function(){
+  return this.sizeOf;
 };
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,26 @@
-var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function(){
+  // Use an object with numeric keys to store values
+  this.storage = {};
+  this.sizeOf = 0; // Hint: set an initial value here
+
+};
+
+
+Stack.prototype.push = function(value){
+  this.storage[this.sizeOf] = value;
+  this.sizeOf += 1;
+};
+
+Stack.prototype.pop = function(){
+  var result;
+  if (this.sizeOf > 0){
+    result = this.storage[this.sizeOf - 1];
+    delete this.storage[this.sizeOf - 1];
+    this.sizeOf --;
+  }
+  return result;
+};
+
+Stack.prototype.size = function(){
+  return this.sizeOf;
 };
