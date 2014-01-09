579f36c02ea321795750e545bdea145a075ef4d4
Hack Reactor Students <willgester@gmail.com>
2014-01-09 11:43:47 -0800

    something done

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,11 +1,22 @@
-var HashTable = function(){
-  this._limit = 8;
+var HashTable = function(limit){
+  this._limit = limit || 8;
+  this._items = 0;
   this._storage = makeLimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   this._storage.set(i, v);
+  this._items += 1;
+
+  if( (this._items / this._limit) >= 0.75 ){
+    this._limit *= 2;
+    var temp = makeLimitedArray(this._limit);
+    this._storage.each(function(value, index){
+      temp.set(index, value);
+    });
+    this._storage = temp;
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -16,5 +27,14 @@ HashTable.prototype.retrieve = function(k){
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   this._storage.set(i, null);
+  if( (this._items / this._limit) <= 0.25 ){
+    this._limit /= 2;
+    var temp = makeLimitedArray(this._limit);
+    this._storage.each(function(value, index){
+      temp.set(index, value);
+    });
+    this._storage = temp;
+  }
+
 };
 
