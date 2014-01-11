125b885ff871b32bb39cbd2c1ff38e52c6a65617
Hack Reactor Students <willgester@gmail.com>
2014-01-10 16:02:22 -0800

    Finished graphing

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,20 +1,48 @@
 var Graph = function(){
+  this.nodes = {};
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  this.nodes[newNode] = {};
+  this.nodes[newNode].edges = [];
+  if (_.keys(this.nodes).length === 2){
+    this.addEdge(newNode, _.keys(this.nodes)[0]);
+  }
+  if (toNode){
+    this.addEdge(newNode, toNode);
+  }
 };
 
 Graph.prototype.contains = function(node){
+  return Boolean(this.nodes[node]);
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.nodes[node];
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
+  return _.contains(this.nodes[fromNode].edges, toNode);
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.nodes[fromNode].edges.push(toNode);
+  this.nodes[toNode].edges.push(fromNode);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  var index = this.nodes[fromNode].edges.indexOf(toNode);
+  if(index !== -1){
+    this.nodes[fromNode].edges.splice(index, 1);
+  }
+  index = this.nodes[toNode].edges.indexOf(fromNode);
+  if(index !== -1){
+    this.nodes[toNode].edges.splice(index, 1);
+  }
+  if (this.nodes[fromNode].edges.length === 0){
+    this.removeNode(fromNode);
+  }
+  if (this.nodes[toNode].edges.length === 0){
+    this.removeNode(toNode);
+  }
 };
