2052680576d08962bdb15ce34770248c474e07e2
Hack Reactor Students <willgester@gmail.com>
2014-01-10 21:01:13 -0800

    finished Bloom

--- sprint-two/spec/bloomFilterSpec.js
+++ sprint-two/spec/bloomFilterSpec.js
@@ -4,7 +4,7 @@ describe("bloomFilter", function() {
   var bf;
 
   beforeEach(function() {
-    bf = new BloomFilter();
+    bf = new BloomFilter(100000);
   });
 
   it("should accept values", function() {
@@ -15,6 +15,37 @@ describe("bloomFilter", function() {
     bf.insert("hello", "world");
     assert.isTrue(bf.retrieve("hello"));
     expect(bf._actualRetrieve("hello")).to.equal("world");
+  });
+
+  it("add items, retrieve items and log the stats", function(){
+    var keys = [];
+    var retrieveAttempts = 10000;
+    var temp;
+    for (var i = 1000; i >= 0; i--) {
+      temp = Math.random().toString();
+      keys.push(temp);
+      bf.insert(temp, i);
+    }
+    //console.log(bf.retrieve(keys[0]));
+
+    var results = [];
+    for( i = retrieveAttempts; i >= 0; i-- ){
+      results.push(bf.retrieve(Math.random().toString()));
+    }
+    keys.forEach(function(value){
+      results.push(bf.retrieve(value));
+    });
+
+    var trues = _.filter(results, function(value){
+      if(value){
+        return true;  
+      }
+      return false;
+    }).length;
+    
+    console.log("Found ", trues, " Matches out of ", retrieveAttempts, " attempts.");
+    console.log("Actual Positive Rate: ", keys.length/trues * 100);
+    console.log("False Positive Rate: ", 100 - (keys.length/trues * 100));
   });
 
 
